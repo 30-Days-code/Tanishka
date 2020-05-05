7c55e9f041b2b1738bc3377fd1586ad9389a4056
Tani21 <53483742+Tani21@users.noreply.github.com>
2020-05-06 00:27:12 +0530

    code changed and driver program removed

--- Day9.cpp
+++ Day9.cpp
@@ -1,71 +1,72 @@
-#include <bits/stdc++.h> 
-  
-using namespace std; 
-  
-#define MAX 1000 
-  
-class Stack { 
-    int top; 
-  
-public: 
-    int a[MAX]; // Maximum size of Stack 
-  
-    Stack() { top = -1; } 
-    bool push(int x); 
-    int pop(); 
-    int peek(); 
-    bool isEmpty(); 
+#include <iostream>
+#include<string>
+using namespace std;
+struct Node { 
+   string data; 
+   struct Node *next; 
 }; 
-  
-bool Stack::push(int x) 
-{ 
-    if (top >= (MAX - 1)) { 
-        cout << "Stack Overflow"; 
-        return false; 
-    } 
-    else { 
-        a[++top] = x; 
-        cout << x << " pushed into stack\n"; 
-        return true; 
-    } 
-} 
-  
-int Stack::pop() 
-{ 
-    if (top < 0) { 
-        cout << "Stack Underflow"; 
-        return 0; 
-    } 
-    else { 
-        int x = a[top--]; 
-        return x; 
-    } 
-} 
-int Stack::peek() 
-{ 
-    if (top < 0) { 
-        cout << "Stack is Empty"; 
-        return 0; 
-    } 
-    else { 
-        int x = a[top]; 
-        return x; 
-    } 
-} 
-  
-bool Stack::isEmpty() 
-{ 
-    return (top < 0); 
-} 
-  
-// Driver program to test above functions 
-int main() 
-{ 
-    class Stack s; 
-    s.push(10); 
-    s.push(20); 
-    s.push(30); 
-    cout << s.pop() << " Popped from stack\n"; 
-  
-    return 0; 
-} 
+struct Node* top = NULL; 
+void push(string val) {
+   struct Node* newnode = (struct Node*) malloc(sizeof(struct Node)); 
+   newnode->data = val; 
+   newnode->next = top; 
+   top = newnode; 
+}
+void pop() {
+   if(top==NULL)
+      cout<<"Stack Underflow"<<endl;
+   else {
+      cout<<"The popped element is "<< top->data <<endl;
+      top = top->next;
+   }
+}
+void display() {
+   struct Node* ptr;
+   if(top==NULL)
+      cout<<"stack is empty";
+   else {   
+      ptr = top; 
+      cout<<"Stack elements are: ";
+      while (ptr != NULL) { 
+         cout<< ptr->data <<" "; 
+         ptr = ptr->next; 
+      } 
+   }
+   cout<<endl;
+}
+int main() {
+   int ch; 
+   string val; 
+   cout<<"1) Push in stack"<<endl;
+   cout<<"2) Pop from stack"<<endl;
+   cout<<"3) Display stack"<<endl;
+   cout<<"4) Exit"<<endl;
+   do {
+      cout<<"Enter choice: "<<endl;
+      cin>>ch;
+      switch(ch) {
+         case 1: {   
+            cout<<"Enter name and pin code of city to be pushed[eg:city-pincode]:"<<endl;
+            cin>>val;
+            push(val);
+            break;
+         }
+         case 2: {
+            pop();
+            break;
+         }
+         case 3: {
+            display();
+            break;
+         }
+         case 4: {
+            cout<<"Exit"<<endl;
+            break;
+         }
+         default: {
+            cout<<"Invalid Choice"<<endl;
+         }
+      }
+   }while(ch!=4); 
+      return 0;
+}  
