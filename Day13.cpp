8cd213e74f362bb2ae5b9293c9656a84ed8a67a2
Tani21 <53483742+Tani21@users.noreply.github.com>
2020-05-12 06:33:36 +0530

    Floyid's Triangle added

--- Day13.cpp
+++ Day13.cpp
@@ -1,2 +1,23 @@
-will be updated within the next 6 hours
-stay tuned
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int rows, number = 1;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for(int i = 1; i <= rows; i++)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            cout << number << " ";
+            number++;
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
