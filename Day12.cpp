c906f518171d2f761ee3c8621f28461adac8feb9
Tani21 <53483742+Tani21@users.noreply.github.com>
2020-05-14 04:28:49 +0530

    various functions added

--- Day12.cpp
+++ Day12.cpp
@@ -1,1 +1,128 @@
-.
+//Program to write in a text file
+#include <fstream> 
+#include<iostream>
+using namespace std; 
+ 
+int main()
+  {  
+     ofstream fout; 
+     fout.open("out.txt"); 
+     char str[300]="Share whatever you feel, but with the right person. Your life would become amazing."; 
+ 
+     fout.close();     
+     return 0; 
+  } 
+
+//Program to read from text file and display it 
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main()
+{ 
+    ifstream fin;
+    fin.open("out.txt");          
+    char ch;          
+    while(!fin.eof())     
+    {          
+      fin.get(ch);          
+      cout<<ch;     
+    }          
+  
+  fin.close();     
+  return 0; 
+}   
+
+//Program to count number of characters. 
+#include<fstream>
+#include<iostream>
+using namespace std; 
+ 
+int main() 
+{     
+  ifstream fin;     
+  fin.open("out.txt"); 
+ 
+   int count=0;     
+   char ch;  
+ 
+    while(!fin.eof())     
+    {         
+      fin.get(ch);         
+      count++;     
+    }          
+  cout<<"Number of characters in file are "<<count;          
+  fin.close();     
+  return 0; 
+} 
+
+//Program to count number of words 
+#include<fstream>
+#include<iostream> 
+using namespace std; 
+ 
+int main() 
+{     
+  ifstream fin;     
+  fin.open("out.txt"); 
+ 
+    int count=0;     
+    char word[30];  
+ 
+    while(!fin.eof())     
+    {         
+      fin>>word;         
+      count++;     
+    }          
+  cout<<"Number of words in file are "<<count;          
+  fin.close();     
+  return 0; 
+} 
+
+//Program to count number of lines 
+#include<fstream> 
+#include<iostream> 
+using namespace std; 
+ 
+int main() 
+{     
+  ifstream fin;     
+  fin.open("out.txt"); 
+ 
+    int count = 0;     
+    char str[80];
+  while(!fin.eof())   
+  {         
+    fin.getline(str,80);       
+    count++;   
+  }          
+  cout<<"Number of lines in file are"<<count; 
+    fin.close();   
+    return 0;
+} 
+
+//Program to copy contents of file to another file. 
+#include<iostream>
+#include<fstream>
+using namespace std; 
+ 
+int main() 
+{     
+  ifstream fin;     
+  fin.open("out.txt");          
+  ofstream fout;     
+  fout.open("sample.txt");          
+  char ch;          
+  while(!fin.eof())     
+  {         
+    fin.get(ch);         
+    fout << ch;     
+  } 
+ 
+    fin.close();     
+    return 0;
+} 
+ 
+ 
+
+ 
