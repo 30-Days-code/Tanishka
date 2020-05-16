ded20ff642099f835620efcc24cfdd8c0ec29334
Tani21 <53483742+Tani21@users.noreply.github.com>
2020-05-16 17:50:31 +0530

    Linked List

---
+++ Day15.cpp
@@ -0,0 +1,25 @@
+/*And the Problem Statement is -
+
+class Queue
+{
+
+  int Data[10];
+  int Front, Rear;
+
+  public :
+
+  Queue( ) 
+  { 
+       Front = - 1;  
+       Rear = - 1; 
+  }
+
+  void Add(); //to add an element into the queue
+
+  void Remove(); //to remove an element from the front of the queue
+
+  void Delete(int ITEM); //to delete all elements whose Data is equal to ITEM
+
+};
+
+Complete the class with all function definitions for a Circular Array Queue. Use another queue to transfer data temporarily.*/
