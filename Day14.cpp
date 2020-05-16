c5bfe23735d25940a052b14c5850f74e2d5ea153
Tani21 <53483742+Tani21@users.noreply.github.com>
2020-05-16 17:49:23 +0530

    Using Templates

---
+++ Day14.cpp
@@ -0,0 +1,6 @@
+/*Write a program using C++ template in which 
+you have to create a function that accepts
+both character type or integer type arrays, 
+respectively, and prints the content of the
+array as output. [ NOTE : Use only ONE variable
+to accept for both the type of arrays (No two separate variables) ]*/
