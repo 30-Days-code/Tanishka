2378833fae445e3121f442653c080826390adb6c
Tani21 <53483742+Tani21@users.noreply.github.com>
2020-05-16 17:51:54 +0530

    exception handling

---
+++ Day17.cpp
@@ -0,0 +1,10 @@
+/*Write a program in C++ using exception handling 
+in which you have to ask the user to enter the 
+marks of a Student for subjects like English,
+Chemistry, ADS, and Maths. If the user exceeds
+the range of marks (if below 0 or above 100) in 
+a particular subject, the program should throw 
+an error “Invalid Marks” to the user and repeat
+the above processes until the use enters the correct
+marks. [NOTE : Try to use try{} block, catch{} block,
+throw() function (all of them, if possible) in your program]*/
