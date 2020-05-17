16d914926844620333f652ef0fb7eec2d9df6241
Tani21 <53483742+Tani21@users.noreply.github.com>
2020-05-17 22:54:53 +0530

    code added

--- Day17.cpp
+++ Day17.cpp
@@ -8,3 +8,50 @@ an error “Invalid Marks” to the user and repeat
 the above processes until the use enters the correct
 marks. [NOTE : Try to use try{} block, catch{} block,
 throw() function (all of them, if possible) in your program]*/
+
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+  int maths, ads, english, chemistry;
+  double average;
+  cout<<"enter maths marks: ";
+  cin>>maths;
+  cout<<"enter ads marks: ";
+  cin>>ads;
+  cout<<"enter english marks: ";
+  cin>>english;
+  cout<<"enter chemistry marks: ";
+  cin>>chemistry;
+
+try {
+  
+  if (maths < 100 && maths>0) 
+  {
+   if(ads < 100 && maths>0)
+   {
+     if(english < 100 && maths>0)
+     {
+       if(chemistry < 100 && maths>0)
+       {
+         average=(maths+ads+english+chemistry)/4;
+         cout<<"your average marks are "<<average;
+       }
+     }
+   }
+  }
+  
+   else {
+    throw (maths);
+  }
+}
+catch (int myNum) {
+  cout << "Please enter valid marks";
+ 
+} 
+
+}
