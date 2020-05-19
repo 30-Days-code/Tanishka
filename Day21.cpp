45f7ba163c70db4f6fa8606fb01872d6f8e96fb1
Tani21 <53483742+Tani21@users.noreply.github.com>
2020-05-19 23:46:44 +0530

    goto statement

---
+++ Day21.cpp
@@ -0,0 +1,22 @@
+/*Write a program in C++ using goto statements 
+that calculates the occupancy rate for a 
+hotel in which the program will ask the user, 
+how many floors the hotel has, and then the 
+number of rooms on each floor and how many 
+of them are occupied on that floor.
+
+After that, the program should display the
+total number of rooms the hotel has, the total
+number of rooms that are occupied, the total 
+number of rooms that are unoccupied, and the 
+percentage of the total rooms that are occupied.
+
+NOTE :-
+
+1. It is a tradition that most hotels do not
+have a thirteenth floor so the program should 
+skip the entire thirteenth floor.
+
+2. Do not accept a value less than 1 for the 
+number of floors. Do not accept a number less
+than 5 for the number of rooms on a floor.*/
