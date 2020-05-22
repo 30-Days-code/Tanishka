63a38ef3a3fc27cb0025ed3ee01d0bc9cecefeb9
Tani21 <53483742+Tani21@users.noreply.github.com>
2020-05-22 23:46:28 +0530

    bitwise operators

---
+++ Day24.cpp
@@ -0,0 +1,8 @@
+/* Write a program in C++ using bitwise operators 
+which should demonstrate the basic functionalities
+of all the 6 bitwise operators in 6 different functions.
+
+[NOTE: Use the 1st link to understand the basics of
+all the 6 bitwise operators. If want to understand
+more the topic and their implementation i n different
+programs, visit the 2nd and 3rd links] */
